e5575ac96ff9b302c3532fb9434f4ec23fe08c31
K0V0 <kovo1533@gmail.com>
2014-09-01 01:36:25 +0200

    multiple pagination handle

--- application_helper.rb
+++ application_helper.rb
@@ -2,32 +2,32 @@ module ApplicationHelper
 
 	def locale_switcher_link(locale)
 
-		route_name = 			""
-		kaminari_page_param_name = 	Kaminari.config.param_name if Object.const_defined?("Kaminari")
-		helpers_object = 		Rails.application.routes.url_helpers
+		route_name = 		""
 		path_arr = 			Rails.application.routes.recognize_path(request.path).to_a
-		pars =				Array.new(path_arr.length-3)
-		paginates =			true if ((!kaminari_page_param_name.nil?)&&(!params[kaminari_page_param_name].nil?))
-		current_locale = 		I18n.locale
+		path_base = 		Hash[path_arr.take(2)]
+		parshash =			Hash.new(path_arr.length-3)
+		current_locale = 	I18n.locale
 
 		for i in 3..path_arr.length-1 do 
 			if (path_arr[i][0] =~ /_id$/) 
 				class_name = path_arr[i][0].to_s.gsub(/_id$/, "").capitalize 
 			elsif (path_arr[i][0].to_s == "id")
-				class_name = path_arr[1][1].classify
+				class_name = path_arr[0][1].to_s.classify
+			else
+				parshash[path_arr[i][0].to_sym] = path_arr[i][1].to_i
 			end
 			if class_name
 				tmp = class_name.constantize.find(path_arr[i][1])
 				nova_pth = tmp.try("slug_" + locale.to_s)
 				if nova_pth
-					pars[i-3] = nova_pth
+					parshash[path_arr[i][0].to_sym] = nova_pth
 				else
 					I18n.locale = locale
 					nova_pth_2 = tmp.try("slug")
 					if nova_pth_2 
-						pars[i-3] = tmp.slug
+						parshash[path_arr[i][0].to_sym] = tmp.slug
 					else
-						pars[i-3] = tmp.id
+						parshash[path_arr[i][0].to_sym] = tmp.id
 					end
 					I18n.locale = current_locale
 				end
@@ -38,15 +38,7 @@ module ApplicationHelper
 			route_name = (route.name).to_s.gsub(/_[a-z]+$/, "_#{locale.to_s}") + "_path" if !route.name.blank?
 		end
 
-		if paginates
-			if (pars.blank? || pars.any? { |p| p.nil? })
-				pars[pars.length-1] = params[kaminari_page_param_name] 
-			else
-				pars << params[kaminari_page_param_name]
-			end
-		end
-
-	return helpers_object.send(route_name, *pars)
+	return Rails.application.routes.url_helpers.send(route_name, path_base.merge(parshash))
 	end
 	
 end
